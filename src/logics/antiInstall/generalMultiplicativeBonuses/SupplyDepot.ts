39ee4ddf0e2957482c5d94b582cb5c1a2a60c7e7
X-20A <155217226+X-20A@users.noreply.github.com>
2025-08-14 20:20:29 +0900

    修正: 対集積地補正

--- src/logics/antiInstall/generalMultiplicativeBonuses/SupplyDepot.ts
+++ src/logics/antiInstall/generalMultiplicativeBonuses/SupplyDepot.ts
@@ -39,35 +39,40 @@ export function calc_anti_Supply_Depot_multiplier(
     total *= LC_and_Katsu_improvement_mod;
     total *= Kami_tank_improvement_mod;
 
-    if (Type_3_shell_count) total *= 2.5;
-    if (WG_count) total *= 1.3;
-    if (WG_count >= 2) total *= 1.4;
-    if (total_mortars_count) total *= 1.2;
-    if (total_mortars_count >= 2) total *= 1.3;
-    if (total_Type4_rocket_count) total *= 1.25;
-    if (total_Type4_rocket_count >= 2) total *= 1.5;
-    if (has_special_LC) total *= 1.4;
-    if (special_LC1_count) total *= 1.25;
+    if (WG_count) total *= 1.25;
+    if (WG_count >= 2) total *= 1.3;
+    
+    if (total_Type4_rocket_count) total *= 1.2;
+    if (total_Type4_rocket_count >= 2) total *= 1.4;
+
+    if (total_mortars_count) total *= 1.15;
+    if (total_mortars_count >= 2) total *= 1.2;
+
+    if (has_special_LC) total *= 1.7;
+    if (special_LC1_count) total *= 1.2;
+    if (Landing_force_count) total *= 1.3;
+    if (
+        Landing_force_count + chiha_count + chiha_kai_count >= 2
+    ) total *= 1.6;
     if (
         m4a1_count + chiha_kai_count + J_tank_count
-    ) total *= 1.1;
-    if (African_tank_count) total *= 1.5;
-    if (Landing_force_count) total *= 1.5;
-    if (Landing_force_count + chiha_count + chiha_count >= 2) total *= 1.3;
-    if (Amphibious_tank_count) total *= 1.5;
+    ) total *= 1.2;
+    if (African_tank_count) total *= 1.3;
+    if (Amphibious_tank_count) total *= 1.7;
     if (
         Amphibious_tank_count >= 2 ||
         Katsu_tank_kai_count
-    ) total *= 1.2;
-    if (seaplane_bomber_count) total *= 1.2;
-    if (armed_boats_count) total *= 1.1;
+    ) total *= 1.5;
+
+    if (armed_boats_count) total *= 1.5;
     if (
         armed_boats_count >= 2 ||
         Katsu_tanks_count >= 2 // ? 省略できる?
     ) total *= 1.1;
-    if (Armys_count) total *= 1.4;
-    if (Armys_count >= 2) total *= 1.2;
-    if (Armys_count >= 3) total *= 1.1;
+    
+    if (Armys_count) total *= 1.85;
+    if (Armys_count >= 2) total *= 1.45;
+    if (Armys_count >= 3) total *= 1.2;
 
     return total;
 }
